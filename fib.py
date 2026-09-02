017284f7342a96ab6c816a7133e6d4d01ebb2e67
Diya-qwe <diyasree.s2024@vitstudent.ac.in>
2026-09-03 05:20:23 +0530

    new

--- fib.py
+++ fib.py
@@ -1,10 +1,6 @@
-n = int(input("The third pull req Enter numper of terms: "))
-a = 0
-b = 1
+n = int(input("Enter number of terms: "))
+a, b = 0, 1
 for i in range(n):
     print(a, end=" ")
     a, b = b, a + b
-    print("This is main branch")
-    print("r1t1")
-    print("r2d1")
     
